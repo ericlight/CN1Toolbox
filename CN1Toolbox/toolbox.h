0d4a6beace19ed489c5019834a9fde4b9e664742
Eric Dodji <dodericg@yahoo.fr>
2020-11-14 17:48:17 +0000

    Update files URL modifications

--- CN1Toolbox/toolbox.h
+++ CN1Toolbox/toolbox.h
@@ -48,21 +48,21 @@ private:
     const QString CN1EXT_HOMEPAGE="https://ericlight.github.io/CN1Extensions";
     const QString CN1EXT_GITHUBPAGE="https://github.com/ericlight/CN1Extensions";
 
-    const QString APPS_VERSIONS="http://localhost:8080/versions.json";
-    const QString CN1LING_WINDOWS_BIN="http://localhost:8080/cn1ling-windows-bin.zip";
-    const QString CN1LING_MACOS_BIN="http://localhost:8080/cn1ling-macos-bin.zip";
-    const QString CN1LING_LINUX_BIN="http://localhost:8080/cn1ling-linux-bin.zip";
-    const QString CN1EXT_WINDOWS_BIN="http://localhost:8080/cn1ext-windows-bin.zip";
-    const QString CN1EXT_MACOS_BIN="http://localhost:8080/cn1ext-macos-bin.zip";
-    const QString CN1EXT_LINUX_BIN="http://localhost:8080/cn1ext-linux-bin.zip";
+//    const QString APPS_VERSIONS="http://localhost:8080/versions.json";
+//    const QString CN1LING_WINDOWS_BIN="http://localhost:8080/cn1ling-windows-bin.zip";
+//    const QString CN1LING_MACOS_BIN="http://localhost:8080/cn1ling-macos-bin.zip";
+//    const QString CN1LING_LINUX_BIN="http://localhost:8080/cn1ling-linux-bin.zip";
+//    const QString CN1EXT_WINDOWS_BIN="http://localhost:8080/cn1ext-windows-bin.zip";
+//    const QString CN1EXT_MACOS_BIN="http://localhost:8080/cn1ext-macos-bin.zip";
+//    const QString CN1EXT_LINUX_BIN="http://localhost:8080/cn1ext-linux-bin.zip";
 
-//    const QString APPS_VERSIONS="https://raw.githubusercontent.com/ericlight/CN1Toolbox/master/apps-updates/versions.json";
-//    const QString CN1LING_WINDOWS_BIN="https://github.com/ericlight/CN1Toolbox/blob/master/apps-updates/cn1ling-windows-bin.zip?raw=true";
-//    const QString CN1LING_MACOS_BIN="https://github.com/ericlight/CN1Toolbox/blob/master/apps-updates/cn1ling-macos-bin.zip?raw=true";
-//    const QString CN1LING_LINUX_BIN="https://github.com/ericlight/CN1Toolbox/blob/master/apps-updates/cn1ling-linux-bin.zip?raw=true";
-//    const QString CN1EXT_WINDOWS_BIN="https://github.com/ericlight/CN1Toolbox/blob/master/apps-updates/cn1ext-windows-bin.zip?raw=true";
-//    const QString CN1EXT_MACOS_BIN="https://github.com/ericlight/CN1Toolbox/blob/master/apps-updates/cn1ext-macos-bin.zip?raw=true";
-//    const QString CN1EXT_LINUX_BIN="https://github.com/ericlight/CN1Toolbox/blob/master/apps-updates/cn1ext-linux-bin.zip?raw=true";
+    const QString APPS_VERSIONS="https://raw.githubusercontent.com/ericlight/CN1Toolbox/master/apps-updates/versions.json";
+    const QString CN1LING_WINDOWS_BIN="https://github.com/ericlight/CN1Toolbox/blob/master/apps-updates/cn1ling-windows-bin.zip?raw=true";
+    const QString CN1LING_MACOS_BIN="https://github.com/ericlight/CN1Toolbox/blob/master/apps-updates/cn1ling-macos-bin.zip?raw=true";
+    const QString CN1LING_LINUX_BIN="https://github.com/ericlight/CN1Toolbox/blob/master/apps-updates/cn1ling-linux-bin.zip?raw=true";
+    const QString CN1EXT_WINDOWS_BIN="https://github.com/ericlight/CN1Toolbox/blob/master/apps-updates/cn1ext-windows-bin.zip?raw=true";
+    const QString CN1EXT_MACOS_BIN="https://github.com/ericlight/CN1Toolbox/blob/master/apps-updates/cn1ext-macos-bin.zip?raw=true";
+    const QString CN1EXT_LINUX_BIN="https://github.com/ericlight/CN1Toolbox/blob/master/apps-updates/cn1ext-linux-bin.zip?raw=true";
 
 
     inline void launchCN1Linguist();
